f0cc483836adb88aea26dc85f3508351fe5f3074
cyprien-jcd <cyprien.cotinaut@jcd-groupe.fr>
2026-07-27 17:01:26 +0200

    Add scan facture papier

---
+++ GLPIKiosk/Features/DocumentScanner/DocumentScannerView.swift
@@ -0,0 +1,37 @@
+//
+//  Scan.swift
+//  GLPIKiosk
+//
+//  Created by Cyprien Cotinaut on 20/07/2026.
+//
+
+import SwiftUI
+import UIKit
+
+struct CameraView: UIViewControllerRepresentable {
+    @Binding var selectedImage: UIImage?
+    @Environment(\.dismiss) var dismiss
+
+    func makeUIViewController(context: Context) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = context.coordinator
+        return picker
+    }
+
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: Context) {}
+
+    func makeCoordinator() -> Coordinator { Coordinator(self) }
+
+    class Coordinator: NSObject, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+        let parent: CameraView
+        init(_ parent: CameraView) { self.parent = parent }
+
+        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+            if let image = info[.originalImage] as? UIImage {
+                parent.selectedImage = image
+            }
+            parent.dismiss()
+        }
+    }
+}
